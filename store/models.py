8ef59c56b360352153e77d9ceb02796b8af1df97
pasiya <pasiya@gmail.com>
2023-10-24 19:01:47 +0530

    store and carts functionality

--- store/models.py
+++ store/models.py
@@ -1,3 +1,7 @@
+from itertools import product
+from random import choice
+from re import T
+from tkinter import CASCADE
 from django.urls import reverse
 from django.db import models
 from category.models import Category
@@ -22,4 +26,26 @@ class Product (models.Model):
     def __str__(self):
         return self.product_name
 
-    
+class VariationManager(models.Manager):
+    def colors(self):
+        return super (VariationManager, self).filter(variation_category='color', is_active=True)
+    def size(self):
+        return super (VariationManager, self).filter(variation_category='size', is_active=True)
+
+variation_category_choice = (
+    ('color', 'color'),
+    ('size', 'size'),
+)
+
+class Variation(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    variation_category = models.CharField(max_length=100, choices=variation_category_choice)
+    variation_value = models.CharField(max_length=100)
+    is_active = models.BooleanField(default=True)
+    created_date = models.DateField(auto_now=True)
+
+    objects = VariationManager()
+
+    def __str__(self):
+        return self.variation_value
+
