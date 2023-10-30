1660e91385fd9cdce53f3826510400319d38337b
pasiya <pasiya@gmail.com>
2023-10-30 15:58:16 +0530

    handled cart and chekout page for the logged users

--- accounts/views.py
+++ accounts/views.py
@@ -2,12 +2,18 @@
 import email
 from email.message import EmailMessage
 from email.policy import default
+from itertools import product
+from math import prod
+from operator import index
 import re
 from tokenize import Token
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages, auth
 from django.shortcuts import render
+
+from carts.models import CartItem
+from store.models import Variation
 
 from .models import Account
 
@@ -21,6 +27,12 @@ from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 from django.utils.encoding import force_bytes
 from django.contrib.auth.tokens import default_token_generator
 from django.core.mail import EmailMessage
+
+from carts.views import _cart_id
+from carts.models import CartItem, Cart
+import requests
+
+
 
 
 
@@ -82,9 +94,56 @@ def login(request):
         user = auth.authenticate(email=email, password=password,)
 
         if user is not None:
+            try:
+                cart=Cart.objects.get(cart_id=_cart_id(request))
+                is_cart_item_exists = CartItem.objects.filter(cart=cart).exists()
+                if is_cart_item_exists:
+                    cart_item = CartItem.objects.filter(cart=cart)
+                    #getting the product variations by cart ID
+                    product_variation=[]
+                    for item in cart_item:
+                        variation= item.variations.all()
+                        product_variation.append(list(variation))
+#get the cart items from the user to access his  product variations
+                    cart_item = CartItem.objects.filter( user=user)
+                    ex_var_list = []
+                    id = []
+                    for item in cart_item:
+                        existing_variation = item.variations.all()
+                        ex_var_list.append(list(existing_variation))
+                        id.append(item.id)
+
+
+                    for pr in product_variation:
+                        if pr in ex_var_list:
+                            index = ex_var_list.index(pr)
+                            item_id = id[index]
+                            item = CartItem.objects.get(id=item_id)
+                            item.quantity +=1
+                            item.user =user
+                            item.save()
+                        else:
+                            cart_item = CartItem.objects.filter(cart=cart)
+                            for item in cart_item:
+                                item.user = user
+                                item.save()
+
+            except:
+                pass
+
             auth.login(request, user)
             messages.success(request, 'You are now logged in.')
-            return redirect('dashboard')
+            url = request.META.get('HTTP_REFERER')
+            try:
+                query = requests.utils.urlparse(url).query
+    # menna methanin passe checkout ekata paninna one
+                param = dict(x.split('=') for x in query.split('&'))
+                if 'next' in param:
+                    nextPage = param['next']
+                    return redirect(nextPage)
+            except:
+                return redirect('dashboard')
+
         
         else:
             messages.error(request, 'Invalid login credentials')
